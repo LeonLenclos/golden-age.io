2eed3547ddb2f38bc1075bbf405d74e52fd36bed
Léon Lenclos <contact@leonlenclos.net>
2022-10-03 15:02:12 +0200

    ui sounds + shortcuts

--- public/js/main.js
+++ public/js/main.js
@@ -14,8 +14,15 @@ var app = new Vue({
     loading:0
   },
   mounted: function () {
-
-    this.load_sound('theme', 0)
+   fetch('/assets.json')
+   .then(response => response.json())
+   .then(assets => {
+    console.log(assets)
+      let audio_dir = assets.children.find(dir=>dir.name=='audio');  
+      audio_dir.children.forEach(file => {
+        if(file.extension=='.mp3') this.load_sound(file.name);
+      });
+   })
 
     document.addEventListener('keyup', (e)=>{
       return this.on_keyup(e)
@@ -25,52 +32,63 @@ var app = new Vue({
     room(new_room, old_room){
       if(!new_room || !old_room) return;
       if(new_room.turn != old_room.turn){
-        this.play_sound('theme', 0);
+        this.play_sound_once('theme', 0);
       }
       if(new_room.playing != old_room.playing
         && new_room.playing == 'ended'){
         this.stop_sound('theme', 0);
-
+        if(this.who_is(this.id).victory.status=='winner'){
+          this.play_sound('ui-winner');
+        } else {
+          this.play_sound('ui-looser');
         }
+      }
     }
   },
   methods: {
-    play_sound(name, variation){
-      if(this.sounds[name][variation].sound.playing()) return;
-      this.sounds[name][variation].sound.play();
+    get_sound(name, variations, intensity){
+      variations = variations || 0;
+      let variation = Math.floor(Math.random()*variations)
+      let complete_name = `${name}-${variation}`;
+      if(intensity !== undefined) complete_name = `${name}-${intensity}-${variation}`;
+      return this.sounds[complete_name].sound;
     },
-    stop_sound(name, variation){
-      if(!this.sounds[name][variation].sound.playing()) return;
-      this.sounds[name][variation].sound.fade(1, 0, 1000);
+    play_sound(name, variation, intensity){
+      let sound = this.get_sound(name, variation, intensity)
+      sound.play();
     },
-    load_sound(name, variation){
-      if(!this.sounds[name]) this.sounds[name] = [];
-      let filename = `assets/audio/${name}-${variation}.mp3`
-      console.log('loading ...', name, variation)
-      this.sounds[name][variation] = {
-        sound:new Howl({src: [filename]}),
+    play_sound_once(name, variation, intensity){
+      let sound = this.get_sound(name, variation, intensity)
+      if(!sound.playing()) sound.play();
+    },
+    stop_sound(name, variation, intensity){
+      let sound = this.get_sound(name, variation, intensity)
+      if(sound.playing()) sound.fade(1, 0, 1000);
+    },
+    load_sound(name){
+      let filename = `assets/audio/${name}.mp3`
+      let sound = new Howl({src: [filename]});
+      this.sounds[name] = {
+        sound:sound,
         loaded:false,
       }
-      this.sounds[name][variation].sound.on('load', ()=>{
-        console.log('loading OK', name, variation)
-        this.sounds[name][variation].loaded = true;
+      sound.on('load', ()=>{
+        this.sounds[name].loaded = true;
         this.check_loading()
       });
-      this.sounds[name][variation].sound.on('loaderror', (e)=>{
-        console.error('loading ERROR', name, variation);
+      sound.on('loaderror', (e)=>{
+        console.error('loading ERROR: ', filename);
       });
-
-
     },
     check_loading(){
+      let total = 0;
+      total += this.sounds.length;
+      if(total == 0) return 0;
       let loaded = 0;
-      let total = 0
       for (var name in this.sounds) {
-        loaded += this.sounds[name].filter(s=>s.loaded).length;
-        total += this.sounds[name].length;
+        if(this.sounds[name].loaded) loaded++;
       }
-      console.log(total,loaded)
-      this.loading = total/loaded;
+      this.loading = loaded/total;
     },
     join_room(player_name, room_name){
       socket.emit('join', {player:player_name, room:room_name});
@@ -83,6 +101,10 @@ var app = new Vue({
     send_message(msg){
       socket.emit('msg', msg);
     },
+    receive_message(msg, emiter){
+      this.messages.push({msg:msg, emiter:emiter});
+      this.play_sound('ui-chat', 3);
+    },
     inspect(pos){
       this.inspected_pos=pos;
     },
@@ -94,31 +116,39 @@ var app = new Vue({
       if(entities.length == 0) return;
       let entity = entities.find(e=>e.building) || entities[0]
       if(!entity) return;
+      
       if(entity.id == this.selection){
         this.unselect();
       }
       else {
+        this.play_sound('ui-select', 3);
         this.selection = entity?.id;
       }
     },
     unselect(){
+      if(!this.selection) return;
+      this.play_sound('ui-select', 3);
       this.selection = undefined;
     },
     select_next(){
       if(!this.room?.world) return;
       let allies = this.room.world.entities.filter(e=>e.owner==this.id);
-      if(!allies.length) return;
+      if(allies.length == 0) return;
+
+      this.play_sound('ui-select', 3);
       this.selection = allies[this.selection_index%allies.length].id;
       this.selection_index ++;
     },
     creation(type){
       if(!this.selection) return;
       socket.emit('creation', this.selection, type);
-      this.unselect();
+      this.play_sound('ui-target', 3);
+      this.selection = undefined;
     },
     target(pos){
       if(!this.selection) return;
       socket.emit('target', this.selection, pos);
+      this.play_sound('ui-target', 3);
       this.selection = undefined;
     },
     who_is(id){
@@ -131,28 +161,29 @@ var app = new Vue({
       let creations = this.what_is(this.selection)?.creations;
       switch (e.key) {
         case 'a':
+        case 'q':
           if(!creations || !creations[0].possible) break;
           this.creation(creations[0].type)
-          return false;
+          break;
         case 'z':
+        case 'w':
           if(!creations || !creations[1].possible) break;
           this.creation(creations[1].type)
-          return false;
-        case 'Tab':
+          break;
+        case ',':
           this.select_next();
-          e.preventDefault();
-          return false;
+          break;
         case ' ':
           this.unselect();
-          e.preventDefault();
-          return false;
+          break;
         case 'Enter':
           this.$refs.messages.give_focus();
-          e.preventDefault();
-          return false;  
+          break;  
         default:
-          break;
+          return true;
       }
+      e.preventDefault();
+      return false;
     }
   },
 });
@@ -163,7 +194,7 @@ socket.on('connect', function() {
 });
 
 socket.on('msg', function(msg, emiter) {
-  app.messages.push({msg:msg, emiter:emiter});
+  app.receive_message(msg, emiter);
 });
 
 socket.on('room joined', function(state) {
